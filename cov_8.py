cc11f92c22558b33adf6ce0e82146eca0948920b
tfguclu <tfguclu@gmail.com>
2015-11-03 13:18:29 +0300

    new covariance code

---
+++ cov_8.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from prody import *
+from pylab import *
+import numpy
+from os.path import basename
+import fnmatch
+import os
+from os import *
+
+for file in os.listdir('.'):
+    if fnmatch.fnmatch(file, '*.pdb'):
+        pdb = file
+
+for file in os.listdir('.'):
+    if fnmatch.fnmatch(file, '*.dcd'):
+        dcd = file
+
+system = parsePDB(str(pdb))
+
+traj = DCDFile(str(dcd))
+
+traj.link(system)
+
+frames1 = traj[25010:45010]
+#frames2 = traj[65010:85010]
+
+eda1 = EDA('')
+#eda2 = EDA('')
+
+eda1.buildCovariance(frames1)
+#eda2.buildCovariance(frames2)
+
+eda1.calcModes(10000)
+#eda2.calcModes(10000)
+
+saveModel(eda1,str(os.path.splitext(dcd)[0])+"_50_90")
+#saveModel(eda2,str(os.path.splitext(dcd)[0])+"_130_170")
+
+writeNMD(str(os.path.splitext(dcd)[0])+"_50_90", eda1, system)
+#writeNMD(str(os.path.splitext(dcd)[0])+"_130_170", eda2, system)
+
+
+
+import matplotlib.pyplot as plt
+arange = np.arange(eda1.numAtoms())
+cross_correlations = np.zeros((arange[-1]+2, arange[-1]+2))
+cross_correlations[arange[0]+1:,arange[0]+1:] = calcCrossCorr(eda1)
+plt.imshow(cross_correlations), plt.colorbar()
+plt.axis([arange[0]+0.5, arange[-1]+1.5, arange[0]+0.5, arange[-1]+1.5])
+savefig("cc_"+str(os.path.splitext(dcd)[0])+"_50_90", dpi=300, bbox_inches='tight')
+
+plt.clf()
+plt.close()
+
+# import matplotlib.pyplot as plt
+# arange = np.arange(eda2.numAtoms())
+# cross_correlations = np.zeros((arange[-1]+2, arange[-1]+2))
+# cross_correlations[arange[0]+1:,arange[0]+1:] = calcCrossCorr(eda2)
+# plt.imshow(cross_correlations), plt.colorbar()
+# plt.axis([arange[0]+0.5, arange[-1]+1.5, arange[0]+0.5, arange[-1]+1.5])
+# savefig("cc_"+str(os.path.splitext(dcd)[0])+"_130_170", dpi=300, bbox_inches='tight')
+
+
+
